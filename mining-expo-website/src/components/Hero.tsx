9796d03b63fc236324e406f84f5d2c9a09c0b1d5
Luke Cloete <Cloeteluke@gmail.com>
2024-01-26 17:01:17 +0200

    Update styles

--- mining-expo-website/src/components/Hero.tsx
+++ mining-expo-website/src/components/Hero.tsx
@@ -5,7 +5,7 @@ import background from "../../public/hero-background.jpg";
 export default function Hero() {
   return (
     <section>
-      <div className="absolute w-full h-full z-[1] bg-gradient-to-b from-blue-950 via-transparent to-blue-950 opacity-75 brightness-90"></div>
+      <div className="absolute top-0 w-full h-full z-[1] bg-gradient-to-b from-blue-950 via-transparent to-blue-950 opacity-75 brightness-90"></div>
       <Image
         src={background}
         alt="Mountains"
