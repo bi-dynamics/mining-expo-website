4b0dd9856729154110f002117a52a32306e42220
Luke Cloete <Cloeteluke@gmail.com>
2024-02-13 16:01:08 +0200

    Styling changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,7 +93,11 @@ const Navbar = () => {
       <div className="flex flex-row sm:pr-8 w-full items-center justify-between">
         {/* LOGO */}
         <div className="flex shrink-0 items-center justify-center h-full px-10 py-4 bg-gradient-to-r from-white via-white to-transparent">
-          <Image src={ExpoLogo} alt="Mining Expo Logo" height={90} />
+          <Image
+            src={ExpoLogo}
+            alt="Mining Expo Logo"
+            className="h-[60px] w-auto xl:h-[100px]"
+          />
         </div>
         <div className="hidden xl:flex flex-row items-center justify-end gap-8 w-full font-rubik">
           {/* Links */}
