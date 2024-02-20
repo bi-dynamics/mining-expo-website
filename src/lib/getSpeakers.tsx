5b046859ffd93d6988bd0cac16665a77231fa6fb
Luke Cloete <Cloeteluke@gmail.com>
2024-02-20 16:34:51 +0200

    Add firestore function for fetching speakers collection

---
+++ src/lib/getSpeakers.tsx
@@ -0,0 +1,23 @@
+import { db } from "./firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+export interface SpeakerData {
+  id?: string;
+  bio?: string;
+  profilePicture?: string;
+  speakerName?: string;
+  title?: string;
+}
+
+export async function getSpeakers(): Promise<SpeakerData[]> {
+  const querySnapshot = await getDocs(collection(db, "speakers"));
+  const speakers: SpeakerData[] = [];
+
+  querySnapshot.forEach((doc) => {
+    speakers.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+  return speakers;
+}
