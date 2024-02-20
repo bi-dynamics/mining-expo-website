daad09666eff08fcea9945994efc93f8f12c7f82
Luke Cloete <Cloeteluke@gmail.com>
2024-02-20 23:11:42 +0200

    Update Banner component properties

--- src/app/main-event-programme/page.tsx
+++ src/app/main-event-programme/page.tsx
@@ -6,15 +6,13 @@ export const metadata: Metadata = {
   title: "Main Event Programme",
   description:
     "30 - 31 August Our Event Schedule 1st Day 2nd Day 07h30 – 09h00 #Windhoek Show Grounds Office Arrival of Exhibitors 09h00 – 09h10 #Makalani Hall Arrival by H.E. Dr Hage Geingob, President of the Republic of Namibia AU & Namibia Anthems Mr Ricardo Goagoseb - Director of ceremonies AU & Namibia Anthems Mr Ricardo",
+  openGraph: {
+    images: [{ url: "../../opengraph-image.jpg" }],
+  },
 };
 
 const MainEventPage = () => {
-  return (
-    <PageBanner
-      title="Main Event Programme"
-      backgroundImage="/mining-expo-namibia-venue.webp"
-    />
-  );
+  return <PageBanner title="Main Event Programme" />;
 };
 
 export default MainEventPage;
