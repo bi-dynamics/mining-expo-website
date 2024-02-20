bacd8dbbec3631e3b60d6fc71365b87d1367205a
Luke Cloete <Cloeteluke@gmail.com>
2024-02-20 16:40:53 +0200

    Add speakers page

--- src/app/conference-programme/speakers-list/page.tsx
+++ src/app/conference-programme/speakers-list/page.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Metadata } from "next";
+import Speakers from "@/components/Speakers";
+import PageBanner from "@/components/PageBanner";
 
 export const metadata: Metadata = {
   title: "Speakers",
+  description:
+    "Meet the speakers attending the Mining Expo this year. Including, but not limited to: Hon. Tom Alweendo, Aphary Muyongo, Anthony Viljoen, Willy Mertens, Olavi Hangula",
+  openGraph: {
+    images: [{ url: "../../opengraph-image.jpg" }],
+  },
 };
 
 const SpeakersListPage = () => {
-  return <div>SpeakersListPage</div>;
+  return (
+    <div className="flex items-center justify-center flex-col w-full h-full">
+      <PageBanner title="Speakers" />
+      <Speakers />
+    </div>
+  );
 };
 
 export default SpeakersListPage;
