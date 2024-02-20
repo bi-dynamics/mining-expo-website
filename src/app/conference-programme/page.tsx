4aeb0eed6965c128d927fa4d11a2ba93a20f1c57
Luke Cloete <Cloeteluke@gmail.com>
2024-02-20 16:41:25 +0200

    Update metadata

--- src/app/conference-programme/page.tsx
+++ src/app/conference-programme/page.tsx
@@ -3,6 +3,11 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Programme",
+  description:
+    "View the conference programme event schedule for Day 1 & Day 2.",
+  openGraph: {
+    images: [{ url: "../opengraph-image.jpg" }],
+  },
 };
 
 const ConferenceProgrammePage = () => {
