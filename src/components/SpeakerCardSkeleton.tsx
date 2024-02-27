b5854b07f48b57c49dfc7226d3a221a3b2477abd
Luke Cloete <Cloeteluke@gmail.com>
2024-02-27 10:07:39 +0200

    Add loading skeleton for detailed speaker page

---
+++ src/components/SpeakerCardSkeleton.tsx
@@ -0,0 +1,82 @@
+import { Skeleton } from "./ui/skeleton";
+
+function SpeakerCardSkeleton() {
+  return (
+    <section className="py-16 w-full xl:w-[80%] mx-auto flex items-center justify-center flex-wrap gap-8">
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start   justify-start gap-8 p-8 w-full md:w-[45vw] xl:w-[35vw]">
+        <div className="flex flex-col items-center justify-center gap-4 w-full xl:w-1/3">
+          <Skeleton className="h-[180px] w-full xl:w-[80px] rounded-xl" />
+          <Skeleton className="h-[20px] w-full xl:w-[80px] " />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-8 w-[250px] mb-4" />
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[160px]" />
+          <Skeleton className="h-20 w-full xl:w-[250px] mt-8" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SpeakerCardSkeleton;
