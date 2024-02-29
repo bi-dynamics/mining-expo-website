6361c06b3819edde66d0f2ea9d835b4f9cefe68e
Luke Cloete <Cloeteluke@gmail.com>
2024-02-29 15:48:00 +0200

    Add Exhibitors image gallery and loading skeleton components to page

--- src/app/main-event-programme/exhibitors/page.tsx
+++ src/app/main-event-programme/exhibitors/page.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Metadata } from "next";
+import PageBanner from "@/components/PageBanner";
+import { ExhibitorsWithSuspense } from "@/components/ExhibitorsWithSuspense";
+import ExhibitorsSkeleton from "@/components/ExhibitorsSkeleton";
 
 export const metadata: Metadata = {
   title: "Exhibitors",
   description:
     "View all exhibitors attending the Mining Expo & Conference 2024",
-  openGraph: {},
+  openGraph: {
+    images: [{ url: "../../opengraph-image.jpg" }],
+  },
 };
 
 const ExhibitorsPage = () => {
-  return <div>ExhibitorsPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <PageBanner title="Exhibitors" />
+      <Suspense fallback={<ExhibitorsSkeleton />}>
+        <ExhibitorsWithSuspense />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ExhibitorsPage;
