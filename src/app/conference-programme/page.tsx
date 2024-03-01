9afbdbbba0fcaf0efd87ae3035838a9b659b5519
Luke Cloete <Cloeteluke@gmail.com>
2024-03-01 15:17:52 +0200

    Add Programme schedule component and fallback to page

--- src/app/conference-programme/page.tsx
+++ src/app/conference-programme/page.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import PageBanner from "@/components/PageBanner";
+import ProgrammeScheduleSkeleton from "@/components/ProgrammeScheduleSkeleton";
+import { ProgrammeScheduleWithSuspense } from "@/components/ProgrammeScheduleWithSuspense";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Programme",
@@ -11,7 +14,14 @@ export const metadata: Metadata = {
 };
 
 const ConferenceProgrammePage = () => {
-  return <div>ConferenceProgrammePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <PageBanner title="Conference Programme" />
+      <Suspense fallback={<ProgrammeScheduleSkeleton />}>
+        <ProgrammeScheduleWithSuspense />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ConferenceProgrammePage;
