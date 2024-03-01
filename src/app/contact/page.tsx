36c3ea98b35bc210ea7856726bbb10e422d0949e
Luke Cloete <Cloeteluke@gmail.com>
2024-03-01 15:21:07 +0200

    Add components to page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Metadata } from "next";
+import PageBanner from "@/components/PageBanner";
+import Maps from "@/components/Maps";
+import ContactForm from "@/components/ContactForm";
 
 export const metadata: Metadata = {
   title: "Contact",
 };
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="flex flex-col  w-full h-full">
+      <PageBanner title="Contact" />
+      <ContactForm />
+      <Maps />
+    </div>
+  );
 };
 
 export default ContactPage;
