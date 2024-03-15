d5c0e6d434f3b6bef43b5f1db5c98d213dd5d9d0
Luke Cloete <Cloeteluke@gmail.com>
2024-03-15 14:52:29 +0200

    Performance optimizations

--- src/components/IframeVideoComponent.tsx
+++ src/components/IframeVideoComponent.tsx
@@ -1,23 +1,33 @@
-async function IframeVideoComponent() {
-  const src = await getSrc();
+"use client";
+import { useState } from "react";
+
+const IframeVideoComponent = () => {
+  const [showVideo, setShowVideo] = useState(false);
   return (
-    <div className="z-[2] flex items-center justify-center w-[90%] xl:w-[40%] h-fit">
-      <iframe
-        width="760"
-        height="515"
-        src={src}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-        loading="lazy"
-        className="rounded-2xl shadow-2xl"
-      ></iframe>
+    <div className="z-[2] flex items-center justify-center w-[90%] xl:w-[40%] h-full">
+      {showVideo ? (
+        <iframe
+          width="760"
+          height="500"
+          src="https://www.youtube.com/embed/rOlnU5Oeq7w?si=wY6Z_appCAYfZaOa"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          loading="lazy"
+          className="rounded-2xl shadow-2xl"
+        />
+      ) : (
+        // Video Thumbnail
+        <div
+          className="flex items-center justify-center relative bg-center bg-cover bg-no-repeat cursor-pointer bg-[url('https://img.youtube.com/vi/rOlnU5Oeq7w/maxresdefault.jpg')] w-[760px] h-[515px] group rounded-2xl shadow-2xl"
+          onClick={() => setShowVideo(true)}
+        >
+          {/* Play Button */}
+          <div className=" cursor-pointer relative h-16 w-24 rounded-md bg-[rgba(0,0,0,0.7)] hover:bg-[rgba(0,0,0,0.5)] group-hover:bg-[#FF0000] after:block after:absolute after:top-[20px] after:left-[40px] after:mx-auto after:border-dashed after:border-t-[12.5px] after:border-r-[0px] after:border-b-[12.5px] after:border-l-[20px] after:border-t-transparent after:border-r-transparent after:border-b-transparent after:border-l-[rgba(255, 255, 255, 1)]" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default IframeVideoComponent;
-
-function getSrc() {
-  return "https://www.youtube.com/embed/rOlnU5Oeq7w?si=wY6Z_appCAYfZaOa";
-}
