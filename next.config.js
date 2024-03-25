a6018a6fcf77ed2ab56fb3c451574dad46dd3d54
Luke Cloete <Cloeteluke@gmail.com>
2024-03-25 13:53:44 +0200

    Install Bundle Analyzer package

---
+++ next.config.js
@@ -0,0 +1,20 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+            protocol: 'https', 
+            hostname: 'firebasestorage.googleapis.com',
+            port: '',
+            pathname: '/v0/b/mining-expo-bc804.appspot.com/o/**',
+          },
+        ],
+      },
+    };
+    
+    const withBundleAnalyzer = require('@next/bundle-analyzer')({
+      enabled: process.env.ANALYZE === 'true',
+    })
+module.exports = withBundleAnalyzer(nextConfig);
