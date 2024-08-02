b7615dfd21cc89189264f9dcae41883d6cb22400
Luke Cloete <Cloeteluke@gmail.com>
2024-08-02 17:01:19 +0200

    Edit spacing

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -24,17 +24,17 @@ async function Maps() {
             />
           </Link>
 
-          <aside className="flex flex-col gap-4 bg-[#F2F2F2] xl:h-fit w-full xl:w-fit p-10 rounded-xl shadow-xl">
+          <aside className="flex flex-col gap-8 bg-[#F2F2F2] xl:h-fit w-full xl:w-fit p-10 rounded-xl shadow-xl">
             <h2 className="font-poppins text-3xl xl:text-5xl font-bold text-expoBlue xl:mb-6">
               Directions
             </h2>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-2">
               <h4 className="font-bold text-lg text-expoOrange">Venue</h4>
               <p className="font-medium text-lg text-expoBlue">
                 Windhoek Show Grounds
               </p>
             </div>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-2">
               <h4 className="font-bold text-lg text-expoOrange">Address</h4>
               <p className="font-medium text-lg text-expoBlue">
                 Jan Jonker Street, Windhoek, Namibia
