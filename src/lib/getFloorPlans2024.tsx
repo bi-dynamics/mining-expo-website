9a8800ae543411a83fa16c76b7efdd305009f332
Luke Cloete <Cloeteluke@gmail.com>
2024-08-02 17:02:19 +0200

    Add 2024 Floor plans and rework Floor Plans page

---
+++ src/lib/getFloorPlans2024.tsx
@@ -0,0 +1,28 @@
+import { db } from "./firebaseConfig";
+import { collection, getDocs, getDocsFromServer } from "firebase/firestore";
+
+export interface FloorPlanData2024 {
+  id: string;
+  image?: string;
+  alt?: string;
+  exhibitors?: ExhibitorsData[];
+}
+
+export interface ExhibitorsData {
+  image: string;
+  name: string;
+  standNumber: number;
+}
+
+export async function getFloorPlans2024(): Promise<FloorPlanData2024[]> {
+  const querySnapshot = await getDocs(collection(db, "floor_plans_2024"));
+  const floorPlans: FloorPlanData2024[] = [];
+
+  querySnapshot.forEach((doc) => {
+    floorPlans.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+  return floorPlans;
+}
