26d85cebdc95cec5a1cae614e40fe2c1adcbc454
Luke Cloete <Cloeteluke@gmail.com>
2024-10-23 15:30:04 +0200

    Remove API Key

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -7,17 +7,17 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4ieOO-HEl7GyhdDR1aj8_7yRjI5IRinM",
+  apiKey: process.env.API_KEY,
   authDomain: "mining-expo-bc804.firebaseapp.com",
   projectId: "mining-expo-bc804",
   storageBucket: "mining-expo-bc804.appspot.com",
   messagingSenderId: "62716428182",
   appId: "1:62716428182:web:25c49a91cf0b6be8aa67b7",
-  measurementId: "G-J6Q77G3MC4"
+  measurementId: "G-J6Q77G3MC4",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {db};
+export { db };
