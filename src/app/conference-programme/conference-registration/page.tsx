37ce0d63a079b645aeb70e602b00c17df8d7410e
Luke Cloete <Cloeteluke@gmail.com>
2025-04-15 15:07:04 +0200

    Put a notice on conference registrations

--- src/app/conference-programme/conference-registration/page.tsx
+++ src/app/conference-programme/conference-registration/page.tsx
@@ -6,11 +6,10 @@ function delegateRegistrationPage() {
       <PageBanner title="Conference Registration" />
       <section className="flex flex-col gap-16 items-center justify-center w-[90%] xl:w-[80%] h-fit mx-auto py-16">
         <h2 className=" text-5xl font-poppins font-bold text-black text-center">
-          Conference Registration Closed
+          Conference Registration Opening Soon
         </h2>
         <p>
-          Online registrations closed. Physical registrations still possible at
-          conference venue. Entry is free of charge.
+          Online registration are currently closed and will be opening soon.
         </p>
       </section>
     </main>
