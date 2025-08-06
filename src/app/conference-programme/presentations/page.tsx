9cdfbf78ae1c08dc890fa714866666a57974c650
Luke Cloete <Cloeteluke@gmail.com>
2025-08-06 09:54:50 +0200

    Add first 2025 presentation

--- src/app/conference-programme/presentations/page.tsx
+++ src/app/conference-programme/presentations/page.tsx
@@ -6,36 +6,36 @@ import clsx from "clsx";
 
 const pastPresentations = [
   {
-    year: 2024,
+    year: 2025,
     src: [
       {
-        file: "/2024-Presentations/Navigating-the-Green-Shoots-Leveraging-Financial-and-Commodity-Insights-for-Sustainable-Growth-in-Namibias-Mining-Sector.pdf",
-        alt: "Navigating the Green Shoots - Leveraging Financial and Commodity Insights for Sustainable Growth in Namibia's Mining Sector",
+        file: "/2025-Presentations/Financing-the-Future-How-Mining-can-Deliver-Namibias-Development-Promise-Angelique-Peake.pdf",
+        alt: "Financing the Future - How Mining can Deliver Namibia's Development Promise - Angelique Peake",
       },
-      {
-        file: "/2024-Presentations/Restarting-the-Langer-Heinrich-Uranium-mines.pdf",
-        alt: "Restarting the Langer Heinrich Uranium mines",
-      },
-      {
-        file: "/2024-Presentations/NIPDB-Investor-Retention-and-After-Care-Services.pdf",
-        alt: "NIPDB Investor Retention and After Care Services",
-      },
-      {
-        file: "/2024-Presentations/Driving-Local-Content-in-Namibias-Mining-Sector-The-Case-of-Kodo-Drilling.pdf",
-        alt: "Driving Local Content in Namibia's Mining Sector - The Case of Kodo Drilling",
-      },
-      {
-        file: "/2024-Presentations/Deep-Yellow_JB_MH_Development-of-Tumas-Project-in-a-Resurging-Uranium-Market.pdf",
-        alt: "Deep Yellow - Development of the Tumas Project in a resurging uranium market",
-      },
-      {
-        file: "/2024-Presentations/Bannermans-Etango-Project_Surving-the-Uranium-Downtun_Repaired.pdf",
-        alt: "Bannerman's Etango Project - Surving the Uranium Downtun",
-      },
-      {
-        file: "/2024-Presentations/A-New-Dawn-for-Namibias-Extractive-Sector.pdf",
-        alt: "A New Dawn for Namibia's Extractive Sector - Ministry of Mines and Energy",
-      },
+      // {
+      //   file: "/2024-Presentations/Restarting-the-Langer-Heinrich-Uranium-mines.pdf",
+      //   alt: "Restarting the Langer Heinrich Uranium mines",
+      // },
+      // {
+      //   file: "/2024-Presentations/NIPDB-Investor-Retention-and-After-Care-Services.pdf",
+      //   alt: "NIPDB Investor Retention and After Care Services",
+      // },
+      // {
+      //   file: "/2024-Presentations/Driving-Local-Content-in-Namibias-Mining-Sector-The-Case-of-Kodo-Drilling.pdf",
+      //   alt: "Driving Local Content in Namibia's Mining Sector - The Case of Kodo Drilling",
+      // },
+      // {
+      //   file: "/2024-Presentations/Deep-Yellow_JB_MH_Development-of-Tumas-Project-in-a-Resurging-Uranium-Market.pdf",
+      //   alt: "Deep Yellow - Development of the Tumas Project in a resurging uranium market",
+      // },
+      // {
+      //   file: "/2024-Presentations/Bannermans-Etango-Project_Surving-the-Uranium-Downtun_Repaired.pdf",
+      //   alt: "Bannerman's Etango Project - Surving the Uranium Downtun",
+      // },
+      // {
+      //   file: "/2024-Presentations/A-New-Dawn-for-Namibias-Extractive-Sector.pdf",
+      //   alt: "A New Dawn for Namibia's Extractive Sector - Ministry of Mines and Energy",
+      // },
     ],
   },
 ];
@@ -46,7 +46,7 @@ const PresentationsPage = () => {
       <PageBanner title="2024 Presentations" />
       <div className="py-16 gap-16 flex items-center justify-center flex-col w-[90%] lg:w-[60%]  mx-auto">
         <h2 className="text-xl md:text-5xl font-poppins font-bold text-black text-center">
-          Conference Presentations
+          2025 yConference Presentations
         </h2>
 
         <div className="flex flex-col gap-2 items-start justify-center">
@@ -55,6 +55,7 @@ const PresentationsPage = () => {
               <Link
                 href={presentation.file}
                 download={presentation.file}
+                target="_blank"
                 key={index}
                 className={clsx(
                   "w-full flex items-center justify-center shrink group bg-expoBlue hover:bg-expoOrange transition-colors rounded-xl font-semibold",
