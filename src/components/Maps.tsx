ef6db8ec42b604b1858d9dfa444b436eaee00421
Luke Cloete <Cloeteluke@gmail.com>
2026-03-04 11:52:04 +0200

    Update mining expo location iframe

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -9,20 +9,21 @@ async function Maps() {
       <div className="h-full w-full z-[1] bg-expoBlue/80 to-transparent">
         {/* container */}
         <div className="flex flex-col xl:flex-row z-10 items-center  justify-center w-[90%] xl:w-[80%] mx-auto h-full py-8 gap-4 xl:gap-8">
-          <Link
-            href="https://maps.app.goo.gl/sdAnFakCxxNqqWi26"
-            aria-label="Expo Venue Location"
-            referrerPolicy="no-referrer"
-            target="_blank"
-            className=" xl:w-[70%]"
-          >
-            <Image
-              src={Map}
-              width={1200}
-              alt="Mining Expo Venue"
-              className="rounded-xl"
-            />
-          </Link>
+          
+           <div className="w-full h-96 rounded-lg overflow-hidden shadow-lg">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2001.9224857146064!2d17.089950368720206!3d-22.581868729541565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1c0b1b4503f1ec9f%3A0xfc8d09ca66b1b97b!2sMining%20Expo!5e1!3m2!1sen!2sna!4v1772617704207!5m2!1sen!2sna"
+                  width="100%"
+                  height="100%"
+                  style={{ border: 0 }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  title="Mining Expo Location"
+                ></iframe>
+                
+              </div>
+         
 
           <aside className="flex flex-col gap-8 bg-[#F2F2F2] xl:h-fit w-full xl:w-fit p-10 rounded-xl shadow-xl">
             <h2 className="font-poppins text-3xl xl:text-5xl font-bold text-expoBlue xl:mb-6">
